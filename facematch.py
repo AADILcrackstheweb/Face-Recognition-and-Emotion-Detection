aa133533413ddb8ae5e0a2960d790007afd01516
Aadil Arsh.S.R <96214556+AADILcrackstheweb@users.noreply.github.com>
2022-10-15 11:51:00 +0530

    Add files via upload

---
+++ facematch.py
@@ -0,0 +1,13 @@
+import face_recognition
+known_image = face_recognition.load_image_file("bezos.jpg")
+unknown_image = face_recognition.load_image_file("musk1.jpg")
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+print(results)
+known_image = face_recognition.load_image_file("musk1.jpg")
+unknown_image = face_recognition.load_image_file("musk2.jpeg")
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+print(results)
